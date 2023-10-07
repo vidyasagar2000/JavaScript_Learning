fca58d5f4ee173481a080210aa0210ebb1e99c00
unknown <vidya9955297836@gmail.com>
2023-10-07 15:59:41 +0530

    Done till datatypes Comparisions

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,63 @@
+let score = true;
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33a" => NaN -> Not a Number
+// "true" => 1
+// "false" => 0
+// null => 0
+// undeined => NaN
+
+let isLoggedIn = "Vidya";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Vidya" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+// console.log(typeof stringNumber);
+
+// *********************************** Operations *******************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2); // 2 to the power 2
+// console.log(2 ** 3); // 2 to the power 3
+// console.log(2 / 3); // 2 to the power 3
+// console.log(2 % 3); // 2 to the power 3
+
+let str1 = "Hello";
+let str2 = " Vidya";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
